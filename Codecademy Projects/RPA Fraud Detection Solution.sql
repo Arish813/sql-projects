7043869bb51e9ce94902f18f580e351a11c2977e
Arish Ali <arish813@gmail.com>
2020-07-17 16:36:39 +0530

    First Commit

---
+++ Codecademy Projects/RPA Fraud Detection Solution.sql
@@ -0,0 +1,27 @@
+ SELECT *
+ FROM transaction_data
+ LIMIT 10;
+
+ SELECT full_name,email 
+ FROM transaction_data
+ WHERE zip=20252;
+
+ SELECT full_name,email
+ FROM transaction_data
+ WHERE full_name="Art Vandelay" or full_name="Art der Vandelay";
+
+ SELECT full_name,email
+ FROM transaction_data
+ WHERE full_name LIKE "Art%Vandelay";
+
+ SELECT ip_address,email
+ FROM transaction_data
+ WHERE ip_address LIKE "10.%";
+
+ SELECT email
+ FROM transaction_data
+ WHERE email LIKE "%@temp_email.com";
+
+ SELECT *
+ FROM transaction_data
+ WHERE ip_address LIKE "120.%" AND full_name LIKE "John%";
